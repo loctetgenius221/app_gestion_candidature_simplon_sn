b726a12bbd3eb3583351478242633c7517eeabb6
loctetgenius <sagnamoussa546@gmail.com>
2024-06-13 13:53:10 +0000

    Affichage de la liste des formations dans la page formation dans dashboard apprenant

--- app/Http/Controllers/CandidatController.php
+++ app/Http/Controllers/CandidatController.php
@@ -9,7 +9,7 @@ class CandidatController extends Controller
 {
     public function afficher() {
 
-        $formation = Formation::all();
-        return view('candidats.formation');
+        $formations = Formation::all();
+        return view('candidats.formation', compact('formations'));
     }
 }
